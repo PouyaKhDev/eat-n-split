9ae6a9ea9b37890ed673804a2edaa69c870de561
pouyaxanony <pouyakhajavidev@gmail.com>
2024-07-21 17:27:22 +0330

    2: Forms static part done

--- src/App.js
+++ src/App.js
@@ -24,7 +24,11 @@ function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+
+      <FormSplitBill />
     </div>
   );
 }
@@ -61,9 +65,50 @@ function Friend({ friend }) {
 
       {friend.balance === 0 && <p>{`you and ${friend.name} are even`}</p>}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
   );
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👫 Friend Name</label>
+      <input type="text" />
+
+      <label>🖼️ Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💸 Bill value</label>
+      <input type="text" />
+
+      <label>🧍Your expense</label>
+      <input type="text" />
+
+      <label>👫 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who's paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">Friend</option>
+      </select>
+    </form>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
 }
 
 export default App;
